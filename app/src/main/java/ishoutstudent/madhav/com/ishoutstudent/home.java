09af0f7db8dfcc215bf8abd6671634f74441ae12
Madhav Somani <madhavsomani007@gmail.com>
2020-03-17 21:51:45 -0500

    Initial commit

---
+++ app/src/main/java/ishoutstudent/madhav/com/ishoutstudent/home.java
@@ -0,0 +1,12 @@
+package ishoutstudent.madhav.com.ishoutstudent;
+
+import android.app.Activity;
+import android.app.Fragment;
+
+/**
+ * Created by madhav on 09-02-2016.
+ */
+public class home extends Fragment {
+
+
+}
---
+++ app/src/test/java/ishoutstudent/madhav/com/ishoutstudent/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ishoutstudent.madhav.com.ishoutstudent;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
